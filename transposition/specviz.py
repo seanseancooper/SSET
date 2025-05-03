033d36d76e20141acce7ce9d4aebcf01dd94f826
seanseancooper <sean.darwin.cooper@gmail.com>
2025-05-03 06:51:51 -0600

    .

--- transposition/specviz.py
+++ transposition/specviz.py
@@ -1,9 +1,3 @@
 # Transposition Layer: Making the Invisible Perceivable
 #
-# Audio renderings of RF/IF signals (sonification)
-# Spectrogram visualizations
-# Synthetic vision of SAR or IR signals
-# Volumetric/haptic representations (optional / future)
-#
-# Spectrogram visualizations
 # specviz.py: Real-time spectrogram with event annotation.
