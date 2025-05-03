5b5f3d06576b9241181fd61aa503a7dba14b03a2
seanseancooper <sean.darwin.cooper@gmail.com>
2025-05-03 06:52:33 -0600

    organizing...

---
+++ transposition/format_converter.py
@@ -0,0 +1,24 @@
+# Transposition Layer: Making the Invisible 'Perceivable'
+# Converts raw EM data into formats that can be sensed and interacted with...
+
+# TEXT:
+#       JSON
+#       RAW?
+#       CSV
+#       TSV
+# BINARY
+#       raster formats:         jpeg, gif, tiff,
+#       vector formats:         SVG, OL vector,
+#       serialized formats:     pickle, HD5,
+#       data formats:           NetCDF, xioarray, ndarray
+
+# READING FORMATS
+# [FormatReader(format_type, data)]: accept a format_type and read data.
+#       processed = FormatReader(format_type.CSV, data)        # read CSV data into an array
+#
+# Audio renderings of RF/IF signals (sonification)
+# Spectrogram visualizations
+# Synthetic vision of SAR or IR signals
+# Volumetric/haptic representations (optional / future)
+#
+# Synthetic vision of SAR or IR signals
