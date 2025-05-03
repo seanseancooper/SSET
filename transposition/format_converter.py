143d67bbabb6cd296d4418bbbfdbffc6939d4205
seanseancooper <sean.darwin.cooper@gmail.com>
2025-05-03 06:52:43 -0600

    .

--- transposition/format_converter.py
+++ transposition/format_converter.py
@@ -16,9 +16,12 @@
 # [FormatReader(format_type, data)]: accept a format_type and read data.
 #       processed = FormatReader(format_type.CSV, data)        # read CSV data into an array
 #
-# Audio renderings of RF/IF signals (sonification)
-# Spectrogram visualizations
-# Synthetic vision of SAR or IR signals
-# Volumetric/haptic representations (optional / future)
+# read from array: This is the default, core op + JSON
+# read from file of type 'format': Reading is semantic for some formats, and not all data is needed.
+# read from store, database, persist layer: read off MQ, db or other structured store (an S3 bucket)
+
+# WRITING FORMATS
+# [FormatWriter(format_type, format_spec)]: accept a format_type (input), a format spec (output) and write data.
+#       other = FormatProcessor(format_type.OtHeR, processed)
 #
 # Synthetic vision of SAR or IR signals
