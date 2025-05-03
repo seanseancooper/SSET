1b13e1e16487b1de7ab4f88f6afba4e022f22a94
seanseancooper <sean.darwin.cooper@gmail.com>
2025-05-03 06:51:42 -0600

    organizing...

--- transposition/specviz.py
+++ transposition/specviz.py
@@ -1,5 +1,4 @@
 # Transposition Layer: Making the Invisible Perceivable
-# Convert raw EM data into formats that can be sensed and interacted with:
 #
 # Audio renderings of RF/IF signals (sonification)
 # Spectrogram visualizations
