66bf94c329161f379d8381c39597b50a958fd9d8
seanseancooper <sean.darwin.cooper@gmail.com>
2025-05-08 13:55:48 -0600

    added methods to process across collecctions.

--- collections/collections.py
+++ collections/collections.py
@@ -1,14 +1,12 @@
 from core.primitives import Emitter, EMField, SignalEvent, SignalMessage
 from core.signal_model import SignalFrame
 from core.time_frequency_frame import TimeFrequencyFrame
-from typing import Dict, List, Generic, T
+from typing import Optional, Generic, T, Dict, List, Any, Tuple, Union
 
 
 class SignalCollection(Generic[T]):
     def __init__(self, items: List[T]):
         self.items = items
-
-    # add methods in 5/7 collections doc.
 
     def filter_by_time(self, start: float, end: float) -> List[T]:
         return [i for i in self.items if start <= i.timestamp <= end]
@@ -18,26 +16,39 @@ class SignalCollection(Generic[T]):
 
 
 class SignalFrameArray(SignalCollection):
+    # transform according to a function
 
-    def __init__(self, frames: List[SignalFrame], items: List[T]):
+    def __init__(self, items: List[SignalFrame]):
         super().__init__(items)
-        self.frames = frames
 
     def select_range(self, start: float, end: float) -> List[SignalFrame]:
-        return [f for f in self.frames if start <= f.timestamp <= end]
+        return [f for f in self.items if start <= f.timestamp <= end]
+
+    def group_to_time_domain(self):
+        return SignalFrameArray([frame.to_time_domain() for frame in self.items if frame.domain == "frequency"])
+
+    def group_to_frequency_domain(self):
+        return SignalFrameArray([frame.to_frequency_domain() for frame in self.items if frame.domain == "time"])
 
 
 class TimeFrequencyFrameList(SignalCollection):
+    # transform according to a function
 
-    def __init__(self, frames: List[TimeFrequencyFrame], items: List[T]):
+    def __init__(self, items: List[TimeFrequencyFrame]):
         super().__init__(items)
-        self.frames = frames
 
     def select_range(self, start: float, end: float) -> List[TimeFrequencyFrame]:
-        return [f for f in self.frames if start <= f.start_time <= end]
+        return [f for f in self.items if start <= f.start_time <= end]
+
+    def group_slice_time(self, start, end):
+        return TimeFrequencyFrameList([item.slice_time(start, end) for item in self.items if item.domain == "frequency"])
+
+    def group_slice_frequency(self, f_start, f_end):
+        return TimeFrequencyFrameList([item.slice_frequency(f_start, f_end) for item in self.items if item.domain == "time"])
 
 
 class EmitterGroup:
+    # Bias dict comparison (in what way?)
 
     def __init__(self, emitters: Dict[str, Emitter]):
         self.emitters = emitters
@@ -48,29 +59,39 @@ class EmitterGroup:
     def get_emitters_by_platform_type(self, platform_type):
         return [self.emitters[id] for id in self.emitters if self.emitters[id].platform_type is platform_type]
 
-    # Bias dict comparison
     # Bias dict merge
-    # search description field in a collection
+    def merge_biases(self, new_biases: Optional[Dict[str, Any]]):
+        all_biases = {}
+        [all_biases.update(emitter.known_bias) for emitter in self.emitters]
+        all_biases.update(new_biases)
+        return all_biases
+
+    # search description fields in emitters
+    def search_description(self, query: str):
+        return [emitter.id for emitter in self.emitters if emitter.description.re.matches(query)]
+
     # get list of id
+    def list_ids(self):
+        return [emitter.id for emitter in self.emitters]
+
     # get list of description
+    def list_descriptions(self):
+        return [emitter.description for emitter in self.emitters]
 
 
 class EMFieldArray(SignalCollection):
 
-    def __init__(self, fields: List[EMField], items: List[T]):
+    def __init__(self, items: List[T]):
         super().__init__(items)
-        self.fields = fields
 
 
 class SignalEventList(SignalCollection):
 
-    def __init__(self, events: List[SignalEvent], items: List[T]):
+    def __init__(self, items: List[SignalEvent]):
         super().__init__(items)
-        self.events = events
 
 
 class SignalMessageList(SignalCollection):
 
-    def __init__(self, frames: List[SignalMessage], items: List[T]):
+    def __init__(self, items: List[SignalMessage]):
         super().__init__(items)
-        self.frames = frames
